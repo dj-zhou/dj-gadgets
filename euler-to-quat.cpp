c93e8135c93ec6d659a420d42c51d719537ee0af
Dingjiang Zhou <56098520+dj-zhou@users.noreply.github.com>
2021-11-15 23:32:42 -0500

    Add gadget: euler-to-quat (#6)
    * add gadgets: euler-to-quat, and quat-to-euler (using three methods)
    Co-authored-by: Dingjiang Zhou <63653706+dj-zhou-bg@users.noreply.github.com>

---
+++ euler-to-quat.cpp
@@ -0,0 +1,75 @@
+#include <cmath>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+using namespace Eigen;
+
+// example: euler-to-quat -90.000592 -0.196423 -0.848052  -d
+
+int main(int argc, char* argv[]) {
+    if ((argc < 4) || (argc > 5)) {
+        printf("usage:\n    %s yaw(psi) pitch(theta) roll(phi) -r/-d\n",
+               argv[0]);
+        printf("    -r: use radian (default)\n");
+        printf("    -d: use degree\n");
+        return 1;
+    }
+    double yaw = atof(argv[1]);
+    double pitch = atof(argv[2]);
+    double roll = atof(argv[3]);
+
+    bool use_radian = true;
+    if (argc == 5) {
+        if (strcmp(argv[4], "-d") == 0) {
+            use_radian = false;
+        }
+    }
+    if (use_radian) {
+        printf("ZYX Euler (Tait-Bryan) angles [yaw, pitch, roll]: [%.6f "
+               "%.6f %.6f] (rad)\n",
+               yaw, pitch, roll);
+    }
+    else {
+        printf("ZYX Euler (Tait-Bryan) angles [yaw, pitch, roll]: [%.6f "
+               "%.6f %.6f] (deg)\n",
+               yaw, pitch, roll);
+        // convert from degree to radian
+        yaw = yaw / 180.0 * M_PI;
+        pitch = pitch / 180.0 * M_PI;
+        roll = roll / 180.0 * M_PI;
+    }
+
+    // calculate rotation matrix, do not delete
+    // Eigen::Matrix3d R_yaw;
+    // R_yaw = Eigen::AngleAxisd(yaw, Eigen::Vector3d(0, 0, 1));
+    // Eigen::Matrix3d R_pitch;
+    // R_pitch = Eigen::AngleAxisd(pitch, Eigen::Vector3d(0, 1, 0));
+    // Eigen::Matrix3d R_roll;
+    // R_roll = Eigen::AngleAxisd(roll, Eigen::Vector3d(1, 0, 0));
+    // Eigen::Matrix3d R = R_yaw * R_pitch * R_roll;
+
+    // std::cout << "R_yaw:\n" << R_yaw << std::endl;
+    // std::cout << "R_pitch:\n" << R_pitch << std::endl;
+    // std::cout << "R_roll:\n" << R_roll << std::endl;
+    // std::cout << "R = R_yaw * R_pitch * R_roll:\n" << R << std::endl;
+
+    // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+    // (Source code)
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+
+    double w = cr * cp * cy + sr * sp * sy;
+    double x = sr * cp * cy - cr * sp * sy;
+    double y = cr * sp * cy + sr * cp * sy;
+    double z = cr * cp * sy - sr * sp * cy;
+    printf("Quaternion: [w, x, y, z] = [%.6f %.6f %.6f %.6f]\n", w, x, y, z);
+    return 0;
+}
--- quat-to-euler.cpp
+++ quat-to-euler.cpp
@@ -7,9 +7,15 @@
 using namespace std;
 using namespace Eigen;
 
+// example: quat-to-euler 0.707074 -0.006445 0.004021 -0.707099
+
+static double rad2deg(double rad) {
+    return rad / M_PI * 180.0;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 5) {
-        printf("usage: %s <double> <double> <double> <double>\n", argv[0]);
+        printf("usage: %s w(q0) x(q1) y(q2) z(q3)\n", argv[0]);
         return 1;
     }
     double w = atof(argv[1]);
@@ -17,12 +23,13 @@ int main(int argc, char* argv[]) {
     const double y = atof(argv[3]);
     const double z = atof(argv[4]);
 
+    // method 1 problem, [1,0,0,0]-> [nan, nan, nan]
     // check if it is a unit quaternion
     if ((std::fabs(w * w + x * x + y * y + z * z) - 1) > 0.001) {
-        printf("not a unit quaternion\n");
+        printf("not a unit quaternion [%.6f %.6f %.6f %.6f]\n", w, x, y, z);
         return 2;
     }
-    printf("quaternion: [%.4f, %.4f, %.4f, %.4f]\n", w, x, y, z);
+    printf("quaternion [w, x, y, z]: [%.6f %.6f %.6f %.6f]\n", w, x, y, z);
     // find axis and angle
     if (w > 1) {
         w = 1;
@@ -33,20 +40,61 @@ int main(int argc, char* argv[]) {
     double v3 = z / sin(theta / 2.0);
     // check if it is a unit vector
     if ((std::fabs(v1 * v1 + v2 * v2 + v3 * v3) - 1) > 0.001) {
-        printf("not a unit vector\n");
-        return 2;
+        printf("not a unit vector:[%.6f, %.6f, %.6f]\n", v1, v2, v3);
+        return 3;
     }
     // find rotation matrix
     Eigen::Matrix3d rot;
-    rot = Eigen::AngleAxisd(theta, Eigen::Vector3d(x, y, z));
+    // [v1, v2, v3] must be a unit vector
+    rot = Eigen::AngleAxisd(theta, Eigen::Vector3d(v1, v2, v3));
 
     // convert to ZYZ euler
     auto euler = rot.eulerAngles(2, 1, 0);
 
-    double yaw = euler[0] / M_PI * 180.0;
-    double roll = euler[2] / M_PI * 180.0;
-    double pitch = euler[1] / M_PI * 180.0;
-    printf("ZYX euler: yaw = %.3f, pitch = %.3f, roll = %.3f (deg)\n", yaw,
-           pitch, roll);
+    double y1 = rad2deg(euler[0]);
+    double p1 = rad2deg(euler[1]);
+    double r1 = rad2deg(euler[2]);
+    printf(
+        "(method 1) ZYX euler: [yaw, pitch, roll] = [%.6f %.6f %.6f] (deg)\n",
+        y1, p1, r1);
+
+    // method 2:
+    // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+
+    double r2 = rad2deg(atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y)));
+    double p2 = rad2deg(asin(2 * (w * y - x * z)));
+    double y2 = rad2deg(atan2(2 * (x * y + w * z), 1 - 2 * (y * y + z * z)));
+    printf(
+        "(method 2) ZYX euler: [yaw, pitch, roll] = [%.6f %.6f %.6f] (deg)\n",
+        y2, p2, r2);
+
+    // method 3 : quaternion to rotation matrix and rotation amtrix to euler
+    // angles
+
+    double ww = w * w;
+    double xx = x * x;
+    double yy = y * y;
+    double zz = z * z;
+    double wx = w * x;
+    double wy = w * y;
+    double wz = w * z;
+    double xy = x * y;
+    double xz = x * z;
+    double yz = y * z;
+    Eigen::Matrix3d rot3;
+    // clang-format off
+    rot3 << ww + xx - yy - zz, 2 * (xy - wz)    , 2 * (wy + xz), \
+            2 * (xy + wz)    , ww - xx + yy - zz, 2 * (yz - wx), \
+            2 * (xz - wy)    , 2 * (wx + yz)    , ww - xx - yy + zz;
+    // clang-format on
+    auto euler3 = rot3.eulerAngles(2, 1, 0);
+
+    double y3 = rad2deg(euler3[0]);
+    double p3 = rad2deg(euler3[1]);
+    double r3 = rad2deg(euler3[2]);
+    printf(
+        "(method 3) ZYX euler: [yaw, pitch, roll] = [%.6f %.6f %.6f] (deg)\n",
+        y3, p3, r3);
+
     return 0;
 }
