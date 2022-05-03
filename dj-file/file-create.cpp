890f816d01d82deb7e5d3b4957a43a840bb1eb51
Dingjiang Zhou <zhoudingjiang@gmail.com>
2022-05-03 15:19:41 -0400

    use bold cyan color to show the messages

--- dj-file/file-create.cpp
+++ dj-file/file-create.cpp
@@ -645,9 +645,12 @@ void create_CMakeLists_txt(void) {
     }
     fprintf(F, "cmake_minimum_required(VERSION 3.10)\n");
     fprintf(F, "project(cmake-project)\n");
-    fprintf(F, "message(STATUS \"CMake version    : \" ${CMAKE_SYSTEM_VERSION})\n");
-    fprintf(F, "message(STATUS \"Compiler         : \" ${CMAKE_CXX_COMPILER})\n");
-    fprintf(F, "message(STATUS \"Operating System : \" ${CMAKE_SYSTEM})\n");
+    fprintf(F, "string(ASCII 27 Esc)\n\n");
+    fprintf(F, "message(STATUS \"${Esc}[1;36mCMake Version: \" ${CMAKE_VERSION})\n");
+    fprintf(F, "message(STATUS \"CMake System : \" ${CMAKE_SYSTEM})\n");
+    fprintf(
+        F,
+        "message(STATUS \"Compiler     : \" ${CMAKE_CXX_COMPILER} \" \" ${CMAKE_CXX_COMPILER_VERSION}) \"${Esc}[m\"\n");
     fprintf(F, "set(CMAKE_CXX_STANDARD 20)\n");
     fprintf(F, "add_compile_options(\n");
     fprintf(F, "  \"-Wall\"\n");
